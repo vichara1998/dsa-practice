a6d236e3555d98dcc56b91d2df54ad431df17573
Vichara Tharkana <vichara0115@gmail.com>
2026-05-21 12:51:41 +0530

    Create clear and printStack methods

--- src/Stack.java
+++ src/Stack.java
@@ -1,11 +1,19 @@
 public class Stack {
-    Node top ;
+    Node top;
+    private Node temp;
 
-    public void push (int data){
+    public void push(int data) {
         Node node = new Node(data);
-        node.next=top;
-        top=node;
+        node.next = top;
+        top = node;
 
     }
 
+    public void clear() {
+        top = null;
+    }
+
+    public void printStack() {
+        Node temp = top;
+    }
 }
