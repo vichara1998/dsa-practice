dd62cbcd9c4594da9bdb3543f74687b20ffeea43
Vichara Tharkana <vichara0115@gmail.com>
2026-05-21 13:22:24 +0530

    Create Queue Class

---
+++ src/Queue.java
@@ -0,0 +1,19 @@
+public class Queue {
+    Node top;
+
+    public void push(int data){
+        Node node = new Node(data);
+
+        if (top==null){
+            top=node;
+
+        }else{
+            Node temp =top;
+        while (temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next =node;
+        }
+
+    }
+}
